91cb527725dc49d7ca9e8d26572ab894892980f3
Lulopera <71723463+lulopera@users.noreply.github.com>
2021-04-01 22:02:12 -0400

    adding Content to files

--- js/main.js
+++ js/main.js
@@ -3,13 +3,29 @@
 console.log('Hello');
 
 const puzzleBody = document.querySelector(".puzzleBody"),
-      bodyIcon  = document.querySelectorAll(".bodyIcon");
+      bodyIcon  = document.querySelectorAll(".bodyIcon"),
+      dropZones = document.querySelectorAll(".drop-zone"),
+      dragZone = document.querySelector(".bodyiconsZone");
 
 function dragStart(event) {
 	console.log('started draggin');
 	event.dataTransfer.setData("savedID", this.id);
 }
 
+function draggedOver(event) {
+		event.preventDefault();
+		console.log('dragged over me');
+}
+
+function dropped(event) {
+	event.preventDefault();
+}
+
+
 bodyIcon.forEach(piece => piece.addEventListener("dragstart", dragStart));
+dropZones.forEach(zone => {
+	zone.addEventListener("dragover", draggedOver);
+	zone.addEventListener("drop", dropped);
+})
 
 })();
