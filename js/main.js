22d194486c285d50978cfeb82f28caa4dbfa7182
Mac <mac@Macs-MBP.phub.net.cable.rogers.com>
2021-04-17 18:45:43 -0400

    updating Js file

--- js/main.js
+++ js/main.js
@@ -2,56 +2,73 @@
 
   console.log('Hello');
 
-const puzzleBody = document.querySelector(".puzzleBody"),
-      bodyIcon  = document.querySelectorAll(".bodyIcon"),
-      dropZones = document.querySelectorAll(".drop-zone"),
-      dragZone = document.querySelector(".bodyiconsZone");
+  const puzzleBody = document.querySelector(".puzzleBody"),
+        bodyIcon  = document.querySelectorAll(".bodyIcon"),
+        dropZones = document.querySelectorAll(".drop-zone"),
+        dragZone = document.querySelector(".bodyiconsZone"),
+        theAudio = document.querySelector('audio');
 
+  function playAudio() {
+   theAudio.play();
+  }
 
-  function dragStart() {
+// loading the audio
+  function loadAudio(sound)
+  {
+    // let targetAudio = this.dataset.trackref;
+    // theAudio.src = `audio/${targetAudio}.mp3`;
+    // theAudio.volume = 0.8;
+    let targetAudio = this.dataset.trackref;
+    theAudio.src = `audio/${targetAudio}.mp3`;
+    theAudio.volume = 0.8;
+    theAudio.load();
+    playAudio();
+  }
+
+  function dragStart(event) {
     console.log('started draggin');
+    // take the dragged image and move it into the drop zone
+    // move it from the left container to the drop zone (reparent it)
     event.dataTransfer.setData("savedID", this.id);
   }
 
   function draggedOver(event) {
     event.preventDefault();
     console.log('dragged over me');
+    // allow an element to be dragged over and then dropped
   }
 
   function dropped(event) {
     event.preventDefault();
-    console.log('dropped something on me');
-    theAudio.load();
-    playAudio();
+     
+
+     if (this.childNodes.length >0) {return;} 
+true
+     let targetID = event.dataTransfer.getData("savedID");
+     console.log('i dragged this image:', targetID);
+    
+    
+    event.target.appendChild(document.querySelector(`#${targetID}`));
   }
 
+dropZones.forEach(zone => {
+      if (zone.childNodes.length >0) {
+        dragZone.appendChild(zone.firstElementChild);
+      }
+    });
 
-  bodyIcon.forEach(piece => piece.addEventListener("dragstart", dragStart));
+bodyIcon.forEach(piece => piece.addEventListener("dragstart", dragStart));
   dropZones.forEach(zone => {
     zone.addEventListener("dragover", draggedOver);
     zone.addEventListener("drop", dropped);
+    // zone.addEventListener("drop", loadAudio);
 })
 
 
 // Audio portion
-let theAudio = document.querySelector("audio"),
-    iconImg = document.querySelectorAll(".bodyIcon");
 
-function playAudio()
-{
-  theAudio.play();
-}
-
-// loading the audio
-  function loadAudio()
-  {
-    let targetAudio = `audio/${targetAudio}.mp3`;
-    theAudio.src = targetAudio;
-    theAudio.volume = 0.8;
-    // // debugger;
-    // theAudio.load();
-    // playTrack();
-    debugger;
-  }
+// dropZones.forEach(zone => zone.addEventListener('drop', loadAudio));
+bodyIcon.forEach(piece => piece.addEventListener("click", loadAudio));
+dropZones.forEach(zone => zone.addEventListener("drop", loadAudio));
 
 })();
