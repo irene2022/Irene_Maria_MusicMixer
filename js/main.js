952e18d79ac87b8c438183ac80b63377762006ce
Mac <mac@Macs-MBP.phub.net.cable.rogers.com>
2021-04-17 18:34:29 -0400

    updating Html file

--- js/main.js
+++ js/main.js
@@ -14,23 +14,44 @@ const puzzleBody = document.querySelector(".puzzleBody"),
   }
 
   function draggedOver(event) {
-		event.preventDefault();
-		console.log('dragged over me');
+    event.preventDefault();
+    console.log('dragged over me');
   }
 
   function dropped(event) {
     event.preventDefault();
     console.log('dropped something on me');
+    theAudio.load();
+    playAudio();
   }
 
 
   bodyIcon.forEach(piece => piece.addEventListener("dragstart", dragStart));
   dropZones.forEach(zone => {
-  	zone.addEventListener("dragover", draggedOver);
-  	zone.addEventListener("drop", dropped);
+    zone.addEventListener("dragover", draggedOver);
+    zone.addEventListener("drop", dropped);
 })
-  
 
-  // debugger;
+
+// Audio portion
+let theAudio = document.querySelector("audio"),
+    iconImg = document.querySelectorAll(".bodyIcon");
+
+function playAudio()
+{
+  theAudio.play();
+}
+
+// loading the audio
+  function loadAudio()
+  {
+    let targetAudio = `audio/${targetAudio}.mp3`;
+    theAudio.src = targetAudio;
+    theAudio.volume = 0.8;
+    // // debugger;
+    // theAudio.load();
+    // playTrack();
+    debugger;
+  }
 
 })();
