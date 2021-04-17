187240afd0dcf3c7be9a43187ce9b437c81607b4
irene2022 <langles47363@gmail.com>
2021-04-17 22:26:11 +0530

    audio codeline added. need to modify so the track changes with the icon. new audio files added

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@
 const puzzleBody = document.querySelector(".puzzleBody"),
       bodyIcon  = document.querySelectorAll(".bodyIcon"),
       dropZones = document.querySelectorAll(".drop-zone"),
-      dragZone = document.querySelector(".bodyiconsZone");
+      dragZone = document.querySelector(".bodyiconsZone"),
+      theAudio = document.querySelector('audio');
 
 
   function dragStart() {
@@ -21,6 +22,8 @@ const puzzleBody = document.querySelector(".puzzleBody"),
   function dropped(event) {
     event.preventDefault();
     console.log('dropped something on me');
+    // debugger;
+    // playAudio();
   }
 
 
@@ -29,8 +32,29 @@ const puzzleBody = document.querySelector(".puzzleBody"),
   	zone.addEventListener("dragover", draggedOver);
   	zone.addEventListener("drop", dropped);
 })
-  
 
-  // debugger;
+
+// Audio portion
+// let theAudio = document.querySelector("audio");
+
+function playAudio()
+{
+  theAudio.play();
+}
+
+// loading the audio
+  function loadAudio(sound)
+  {
+    let targetAudio = this.dataset.trackref;
+    theAudio.src = `audio/${targetAudio}.mp3`;
+    theAudio.volume = 0.8;
+    theAudio.load();
+    playAudio();
+  }
+
+for (icon of bodyIcon)
+{
+  icon.addEventListener("drop", loadAudio);
+}
 
 })();
